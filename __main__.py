a737e8a7ecfcd44c26a6346758f42deb7006b0ec
russell <rrlittle2@gmail.com>
2016-06-21 11:44:13 -0500

    began __main__.py which will be responsible for orchestrating the whole program

--- __main__.py
+++ __main__.py
@@ -1,4 +1,27 @@
+''' this file contains the main stuff to actuall run this package.
+	a user can run this from oe directory higher by entering `python edfdat`
+	that will run the package, and __main__.py is the entrypoint.
+
+	this prompts the user for edf files and mapping files then parses 
+	it into a datfile.
+
+	then it will drop you into an ipython interpreter so you can do whatever. 
+	'''
+
 import IPython
-from __init__ import primlog, rglog, datumlistlog, datumlog, dsetlog
-from datumobjects import datum, datumlist
+from loggers import primlog, rglog, datumlistlog, datumlog, dsetlog
+from datumobjects import datum
+import datfile
+
+def process_edf_file():
+	''' this function prompts the user for an edf file and for 
+		a mappings file to convert the edf files into a datfile. 
+
+	'''
+	pass 
+
+
+if __name__ == '__main__': 
+	process_edf_file()
+
 IPython.embed(local_ns = locals())
